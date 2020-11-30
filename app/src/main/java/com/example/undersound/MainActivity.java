a5f4e65fcb46f99fedf2c6934c423ef29e0ed886
SashaDolynuk <sasha.dolynuk12@gmail.com>
2020-11-30 00:23:00 -0500

    updated SearchActivity- now the result is launched

--- app/src/main/java/com/example/undersound/MainActivity.java
+++ app/src/main/java/com/example/undersound/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.undersound;
 import android.app.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends Activity implements OnClickListener {
 
@@ -17,7 +19,8 @@ public class MainActivity extends Activity implements OnClickListener {
     private Button country;
     private Button jazz;
 
-    private String genre;
+    public static final String TAG_GENRE = "genre";
+    private static final String TAG_DEBUG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +47,7 @@ public class MainActivity extends Activity implements OnClickListener {
      * as buttons are children of the view class, buttons can polymorphically be passed in. The button
      * that called the onClick is automatically fed in*/
     public void onClick(View v) {
-
+        String genre = "";
         //The switch statements grab the id values of the button pressed and calculates the tip accordingly
         switch (v.getId()) {
 
@@ -77,6 +80,24 @@ public class MainActivity extends Activity implements OnClickListener {
             }
         }
 
+        launchResultActivity(genre);
+    }
 
+    private void launchResultActivity(String genre)
+    {
+        /*The intent class represents an action is used to "load" activities into a variable so they can be passed in and launched from
+         * the startActivity method. Basic intents take two arguments, the current class(.java) and the class(.java) that the app will move to
+         *  The line below initializes an Intent named resultActivity and passes in (Main.this,Result.class) much like the this-> pointer in C++,
+         *  the this keyword in java is used by classes to reference themselves*/
+        Intent resultActivity = new Intent(MainActivity.this, SearchActivity.class);
+
+        /*Since this method is private, if we want the Result Activity/class to access it's members (the strings TAG_TIP and TAG_GRAND_TOTAL),
+         *we can "push" members from the Main Acivity/class to Result, much like how a friend function can "pull" private members from objects
+         */
+
+        resultActivity.putExtra(TAG_GENRE, genre);
+        Log.d(TAG_DEBUG, "Genre: " + genre);
+        //Launches the new activity
+        startActivity(resultActivity);
     }
 }
--- app/src/main/java/com/example/undersound/SearchActivity.java
+++ app/src/main/java/com/example/undersound/SearchActivity.java
@@ -3,12 +3,29 @@ package com.example.undersound;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
+    TextView genreText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        genreText = (TextView)findViewById(R.id.genre);
+        initializeTextViews();
+    }
+
+    private void initializeTextViews()
+    {
+        //Sets doubles according to the values "pushed" from Main.java
+        String genre = getIntent().getExtras().getString(MainActivity.TAG_GENRE);
+
+        //Sets the strings accordingly
+        String currentGenre = genreText.getText().toString();
+
+        //Sets the texts to display the values
+        genreText.setText(currentGenre);
     }
 }
