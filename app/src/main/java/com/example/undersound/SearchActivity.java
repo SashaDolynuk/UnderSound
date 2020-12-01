3483e17c3e60ad45f9af3607f70dcba82dcf1d08
sunwoopk <73982673+sunwoopk@users.noreply.github.com>
2020-11-30 23:48:26 -0500

    added dependencies

--- app/src/main/java/com/example/undersound/SearchActivity.java
+++ app/src/main/java/com/example/undersound/SearchActivity.java
@@ -5,6 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SearchActivity extends AppCompatActivity {
     TextView genreText;
 
