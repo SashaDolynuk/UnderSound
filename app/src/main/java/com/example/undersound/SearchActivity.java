6f9dff6f5f476295ef701af2e43cba7d576de5c9
malaika-99 <74626539+malaika-99@users.noreply.github.com>
2020-12-01 22:03:49 -0500

    Second Page outline.

--- app/src/main/java/com/example/undersound/SearchActivity.java
+++ app/src/main/java/com/example/undersound/SearchActivity.java
@@ -5,56 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class SearchActivity extends AppCompatActivity {
     TextView genreText;
+    TextView artistText;
+    TextView trackText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        genreText = (TextView)findViewById(R.id.genre);
+        genreText = (TextView) findViewById(R.id.editGenre);
+        artistText = (TextView) findViewById(R.id.editArtist);
+        trackText = (TextView) findViewById(R.id.editTrack);
         initializeTextViews();
     }
-//comment
-    private void initializeTextViews()
-    {
+
+    //comment
+    private void initializeTextViews() {
         //Sets doubles according to the values "pushed" from Main.java
         String genre = getIntent().getExtras().getString(MainActivity.TAG_GENRE);
-
+        String artist = getIntent().getExtras().getString(MainActivity.TAG_ARTIST);
+        String track = getIntent().getExtras().getString(MainActivity.TAG_TRACK);
         //Sets the strings accordingly
         String currentGenre = genreText.getText().toString();
-
+        String currentArtist = artistText.getText().toString();
+        String currentTrack = trackText.getText().toString();
         //Sets the texts to display the values
         genreText.setText(genre); //use the string from the other class
+        artistText.setText(artist);
+        trackText.setText(track);
     }
 
     // get request for search for item based on track string that user enters
-    String url = "http://my-json-feed";
-
-    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                @Override
-                public void onResponse(JSONObject response) {
-                    //textView.setText("Response: " + response.toString());
-                }
-            }, new Response.ErrorListener() {
-
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    // TODO: Handle error
-
-                }
-            });
-
 }
