cb09f60e96246366b258b8914836c20cd870d594
SashaDolynuk <sasha.dolynuk12@gmail.com>
2020-12-02 22:14:58 -0500

    json request and edittext edits

--- app/src/main/java/com/example/undersound/SearchActivity.java
+++ app/src/main/java/com/example/undersound/SearchActivity.java
@@ -4,6 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SearchActivity extends AppCompatActivity {
@@ -11,15 +27,56 @@ public class SearchActivity extends AppCompatActivity {
     TextView artistText;
     TextView trackText;
 
+    // pass token into this activity as a string
+    // this is a temporary token
+    String token = "BQAVQg497bjIi-a7P6AUFLkxADwPDfy34WYE0dQCX5XZKt9k7FoItvFEKdONsW6ixInP-a8Qted9QXoO-i8wxXXkZNXb4twE3gEBnQtUVEhDERFwpQ-sPyQdNtR56oeR8dvS9EdEGKamirl6Rw";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        genreText = (TextView) findViewById(R.id.editGenre);
-        artistText = (TextView) findViewById(R.id.editArtist);
-        trackText = (TextView) findViewById(R.id.editTrack);
+        // can only access ids and set/get text from the activity_search.xml file. there is only one textview in there, editGenre2
+        genreText = (TextView) findViewById(R.id.editGenre2);
+
         initializeTextViews();
+
+
+        // search for an item (track) using volley get request
+        // returns json object, parse for track id
+        String track = "watermelon%20sugar"; // figure out how to initialize with "%20" as spaces
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String searchURL = "https://api.spotify.com/v1/search?q=" + track + "&type=track";
+        // StringRequest or JsonObjectRequest
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, searchURL, (JSONObject) null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // response
+                        Log.d("Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        Log.d("ERROR","error => "+error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Accept", "application/json");
+                params.put("Content-Type", "application/json");
+                params.put("Authorization", "Bearer " + token);
+
+                return params;
+            }
+        };
+        queue.add(getRequest);
     }
 
     //comment
@@ -29,13 +86,16 @@ public class SearchActivity extends AppCompatActivity {
         String artist = getIntent().getExtras().getString(MainActivity.TAG_ARTIST);
         String track = getIntent().getExtras().getString(MainActivity.TAG_TRACK);
         //Sets the strings accordingly
-        String currentGenre = genreText.getText().toString();
-        String currentArtist = artistText.getText().toString();
-        String currentTrack = trackText.getText().toString();
+
+        //commented the following out
+
+//        String currentGenre = genreText.getText().toString();
+//        String currentArtist = artistText.getText().toString();
+//        String currentTrack = trackText.getText().toString();
         //Sets the texts to display the values
         genreText.setText(genre); //use the string from the other class
-        artistText.setText(artist);
-        trackText.setText(track);
+//        artistText.setText(artist);
+//        trackText.setText(track);
     }
 
     // get request for search for item based on track string that user enters
